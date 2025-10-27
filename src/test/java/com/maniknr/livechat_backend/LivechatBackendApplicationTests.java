a457b369ad628b8309a79600d675d7e75544ab8a
maniknr02 <manikantamaniknr@gmail.com>
2025-10-27 22:07:54 +0530

    FEAT: Initial code base setup

---
+++ src/test/java/com/maniknr/livechat_backend/LivechatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maniknr.livechat_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivechatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
